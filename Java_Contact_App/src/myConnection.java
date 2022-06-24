1690f4b42bb02c0d7c11f7bf7a49401353a460a4
Deepshikha <76842608+Deepshikha73@users.noreply.github.com>
2022-06-24 22:42:20 +0530

    Add files via upload

---
+++ Java_Contact_App/src/myConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class myConnection {
+    
+    public static Connection getConnection(){
+        Connection con = null;
+        try{
+             Class.forName("com.mysql.cj.jdbc.Driver");
+             con = DriverManager.getConnection("jdbc:mysql://localhost/javacontactsapp","root","");
+        }catch(ClassNotFoundException | SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+       
+        
+        return con;
+    }
+}
